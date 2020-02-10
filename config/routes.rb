3c77597d45864a758c24d29d05bbe8c51fcd73e1
Sarah <sarahahmad17@gmail.com>
2020-02-09 23:42:58 -0500

    created Ideas scaffold, picture uploader, pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'pages/info'
+
+  resources :ideas
+  root to: redirect('/ideas')
+  get 'pages/info'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root 'home#index'
 end
