8cfdd7fb1ff95125dd4554d064f215ad8c33685f
Sarah <sarahahmad17@gmail.com>
2020-02-11 22:28:36 -0500

    bucket name change

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,7 +8,7 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV['S3_SECRET'] # required unless using use_iam_profile
 
   }
-  config.fog_directory  = 'idea_app' # required
+  config.fog_directory  = 'sarah-rehan-idea-app' # required
   config.fog_public     = false                                                 # optional, defaults to true
   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
 end
