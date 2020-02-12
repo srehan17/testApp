c35e63fd3014a0fcaa83b64e3292419e40ec2579
Sarah <sarahahmad17@gmail.com>
2020-02-11 22:35:23 -0500

    setting bucket region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS', # required
     aws_access_key_id: ENV['S3_KEY'], # required unless using use_iam_profile
-    aws_secret_access_key: ENV['S3_SECRET'] # required unless using use_iam_profile
-
+    aws_secret_access_key: ENV['S3_SECRET'], # required unless using use_iam_profile
+    region: 'ca-central-1'
   }
   config.fog_directory  = 'sarah-rehan-idea-app' # required
   config.fog_public     = false                                                 # optional, defaults to true
